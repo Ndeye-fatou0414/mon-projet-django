6c910f08dcaaf01fa44f40730237c0fb03e580df
Ndeye-fatou0414 <fatoundieguene98@gmail.com>
2026-01-28 12:44:55 +0000

    test1

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 def main():
     """Run administrative tasks."""
     # Cette ligne doit avoir exactement 4 espaces au début
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MonProjectDjango.settings')
+   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MonProjectDjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
